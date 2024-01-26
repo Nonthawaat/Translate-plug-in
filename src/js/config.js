b491554b8a48ca9239d1dccbccd6bf70e6efc146
Anoulukk <101563242+Anoulukk@users.noreply.github.com>
2024-01-26 15:21:55 +0700

    pre final code

--- src/js/config.js
+++ src/js/config.js
@@ -3,66 +3,53 @@ jQuery.noConflict();
 (async function ($,Swal10, PLUGIN_ID) {
   "use strict";
 
-    let GETCONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
-    let ROWCOUNT = 1;
+    const GETCONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
     let GETFIELDS = "";
+    let VALIDATEFIELDS = false;
+    let VALIDATELANGUAGELIST = false;
     try {
-        let param = { app: kintone.app.getId() };
-        GETFIELDS = await kintone.api('/k/v1/preview/app/form/fields', 'GET', param);
-    }catch (error) {return console.log('Error', error.message)}
+        GETFIELDS = await kintone.api('/k/v1/preview/app/form/fields', 'GET', { app: kintone.app.getId() });
+    }catch (error) {return console.error('Error', error.message)}
 
-    // ----------------------------setDefaultConfig------------------------------------
+    //SetDefaultConfig
     function setDefaultConfig() {
         try {
-    
-            let config = {};
-            let parsedLanguageListAndFieldItem = [];
-            let parsedLanguageList = [];
-            let parsedFieldItem = [];
-                config = GETCONFIG;
-                parsedLanguageListAndFieldItem = GETCONFIG.config ? JSON.parse(GETCONFIG.config) : [];
-                parsedLanguageList = parsedLanguageListAndFieldItem.languageList;
-                parsedFieldItem = parsedLanguageListAndFieldItem.translateFields;
-          if (parsedLanguageList == undefined) {
-            createNewRow('Default', "");  
-
-          }else{
-            //Set default data
-            let rowToSetLanguageList = 0;
-            let rowToSetFieldItem = 0;
-            for (let i = 0; i < parsedLanguageList.length; i++) {
-                const object = parsedLanguageList[i];
-                object.itemCode;
-                createNewRow('setDefaultLanguageList', "");
-                rowToSetLanguageList++;
-                $('#language-list-tbody > tr:eq(' + rowToSetLanguageList + ') .select_language_column').val(object.languageIso.toLowerCase());
-                $('#language-list-tbody > tr:eq(' + rowToSetLanguageList + ') .language_iso').val(object.languageIso);
-                $('#language-list-tbody > tr:eq(' + rowToSetLanguageList + ') .button_label').val(object.buttonLabel);
+            const parsedLanguageListAndFieldItem = GETCONFIG.config ? JSON.parse(GETCONFIG.config) : [];
+            const parsedLanguageList = parsedLanguageListAndFieldItem.languageList;
+            const parsedFieldItem = parsedLanguageListAndFieldItem.translateFields;
+            if (parsedLanguageList == undefined) {
+                createNewRow('Default', "");  
+            }else{
+                //Set default data
+                let rowToSetLanguageList = 0;
+                let rowToSetFieldItem = 0;
+                for (let i = 0; i < parsedLanguageList.length; i++) {
+                    const object = parsedLanguageList[i];
+                    createNewRow('setDefaultLanguageList', "");
+                    rowToSetLanguageList++;
+                    $('#language-list-tbody > tr:eq(' + rowToSetLanguageList + ') .select_language_column').val(object.languageIso.toLowerCase());
+                    $('#language-list-tbody > tr:eq(' + rowToSetLanguageList + ') .language_iso').val(object.languageIso);
+                    $('#language-list-tbody > tr:eq(' + rowToSetLanguageList + ') .button_label').val(object.buttonLabel);
+                }
+                for (let i = 0; i < parsedFieldItem.length; i++) {
+                    const object = parsedFieldItem[i];
+                    createNewRow('setDefaultTranslateField', "");
+                    rowToSetFieldItem++;
+                    $('#fields-list-tbody > tr:eq(' + rowToSetFieldItem + ') .ITEM_name').val(object.itemCode);
+                    $('#fields-list-tbody > tr:eq(' + rowToSetFieldItem + ') .select_field_column').val(object.targeFields.fieldCode);
+                }
+                $('.js-reflection-button').click();
+                $('.select_default_lang_column').val(parsedLanguageListAndFieldItem.defaultLanguage);
+                checkRowNumber();
             }
-            for (let i = 0; i < parsedFieldItem.length; i++) {
-                const object = parsedFieldItem[i];
-                createNewRow('setDefaultTranslateField', "");
-                rowToSetFieldItem++;
-                $('#fields-list-tbody > tr:eq(' + rowToSetFieldItem + ') .ITEM_name').val(object.itemCode);
-                $('#fields-list-tbody > tr:eq(' + rowToSetFieldItem + ') .select_field_column').val(object.targeFields.fieldName);
-            }
-            $('.js-reflection-button').click();
-            checkRowNumber();
-                
-          }  return
-        }catch (error) {
-            console.log(error);
-        }
+        }catch (error) {console.error(error)}
     }
-    // -----------------------------Create new row------------------------
+    //Create new row
     function createNewRow(type, row) {
-        const rowToCloneLanguageList = $("#language-list-tbody tr:first-child");
-        const rowToCloneTranslateField = $("#fields-list-tbody tr:first-child");
         const rowToSetLanguageList = $("#language-list-tbody tr:last-child");
         const rowToSetTranslateField = $("#fields-list-tbody tr:last-child");
-        const newRowLanguageList = rowToCloneLanguageList.clone(true);
-        const newRowTranslateField = rowToCloneTranslateField.clone(true);
-            ROWCOUNT++;
+        const newRowLanguageList = $("#language-list-tbody tr:first-child").clone(true);
+        const newRowTranslateField = $("#fields-list-tbody tr:first-child").clone(true);
             newRowLanguageList.removeAttr("hidden");
             newRowTranslateField.removeAttr("hidden");
         if (type === "newLanguageList") {
@@ -79,35 +66,27 @@ jQuery.noConflict();
             rowToSetTranslateField.after(newRowTranslateField);
         }
     }
-    //-----------------------------Check row nuber---------------------
+    //Check row number
     function checkRowNumber() {
-        const removeButtons = $("#language-list-tbody > tr .removeRow");
-        const removeButton = $("#fields-list-tbody > tr .removeRow");
-        const rows = $("#language-list-tbody > tr");
-        const row = $("#fields-list-tbody > tr");
-        if (rows.length < 3) {
-            removeButtons.eq(1).css("display", "none");
+        const removeButtonLanguage = $("#language-list-tbody > tr .removeRow");
+        const removeButtonField = $("#fields-list-tbody > tr .removeRow");
+        if ( $("#language-list-tbody > tr").length < 3) {
+            removeButtonLanguage.eq(1).css("display", "none");
         }else {
-            removeButtons.each(function () {
-            $(this).css("display", "inline-block");
-            });
+            removeButtonLanguage.eq(1).css("display", "inline-block");
         }
-        if (row.length < 3) {
-            removeButton.eq(1).css("display", "none");
+        if ($("#fields-list-tbody > tr").length < 3) {
+            removeButtonField.eq(1).css("display", "none");
         }else {
-            removeButton.each(function () {
-            $(this).css("display", "inline-block");
-            });
+            removeButtonField.eq(1).css("display", "inline-block");
         }
     }
-    let VALIDATEFIELDS = false;
-    let VALIDATELANGUAGELIST = false;
-    // ------------------------Function validate--------------------
+    //Function validate Language list and field item list
     function validateLanguageAndFields(type, language, buttonLabel, uniqueLanguages, uniqueButtonLabels, item, field , uniqueITEM, uniqueFields) {
         switch (type) {
             case "reflection":
-        VALIDATELANGUAGELIST = false
-        if (language == "-----") {
+                VALIDATELANGUAGELIST = false
+                if (language == "-----") {
                     showError("The language selection should not be empty");
                     return false;
                 }
@@ -126,8 +105,8 @@ jQuery.noConflict();
                 VALIDATELANGUAGELIST = true
                 break;
             default:
-        VALIDATEFIELDS = false;
-        if (field == "-----") {
+                VALIDATEFIELDS = false;
+                if (field == "-----") {
                     showError("The field selection should not be empty");
                     return false;
                 }
@@ -143,11 +122,12 @@ jQuery.noConflict();
                     showError("Duplicate ITEM name");
                     return false;
                 }
-        VALIDATEFIELDS = true;
+                VALIDATEFIELDS = true;
                 break;
         }
         return true;
     }
+    //Display popup error using sweet alert
     function showError(message) {
         Swal10.fire({
             icon: "error",
@@ -155,15 +135,13 @@ jQuery.noConflict();
             text: message,
         });
     }
-
-    // ------------------------Function get data--------------------
+    // Function get data from LanguageList
     function getLanguageList() {
         let data = [];
         let config = {};
         let uniqueLanguages = [];
         let uniqueButtonLabels = [];
-        let rowNumber = $('#language-list-tbody > tr');
-        for (let row = 1; row < rowNumber.length; row++) {
+        for (let row = 1; row < $('#language-list-tbody > tr').length; row++) {
             let language = $('#language-list-tbody > tr:eq(' + row + ') select[name="select_language_column"] option:selected').text();
             let buttonLabel = $('#language-list-tbody > tr:eq(' + row + ') .button_label').val();
             if (validateLanguageAndFields("reflection",language, buttonLabel, uniqueLanguages, uniqueButtonLabels)) {
@@ -180,14 +158,13 @@ jQuery.noConflict();
         }
         return data;
     }
-
+    // Function get data from FieldItem
     function getFieldItem() {
         let data = [];
         let config = {};
         let uniqueITEM = [];
         let uniqueFields = [];
-        let rowNumber = $('#fields-list-tbody > tr');
-        for (let row = 1; row < rowNumber.length; row++) {
+        for (let row = 1; row < $('#fields-list-tbody > tr').length; row++) {
             let item = $('#fields-list-tbody > tr:eq(' + row + ') .ITEM_name').val();
             let field = $('#fields-list-tbody > tr:eq(' + row + ') select[name="select_field_column"] option:selected').text();
             let fieldCode = $('#fields-list-tbody > tr:eq(' + row + ') select[name="select_field_column"] option:selected').val();
@@ -209,45 +186,47 @@ jQuery.noConflict();
     }
 
     $(document).ready(async function() {
-        // Get the array of language objects
+        const setFields =["SINGLE_LINE_TEXT", "RICH_TEXT", "MULTI_LINE_TEXT"];
+        const FieldSorted = Object.values(GETFIELDS.properties).sort((a, b) => {
+            return a.label.localeCompare(b.label);
+        });
+        //Get the array of language Pack objects
         const languageObjects = langPack();
-       await $.each(languageObjects, function (index, langObject) {
+        //Add option language to dropdown
+        await $.each(languageObjects, function (index, langObject) {
             $('#select_language_column').append($('<option>', {
                 value: langObject.iso,
                 code: langObject.code.toUpperCase(),
                 text: langObject.language
             }));
         });
-        let option = $('<option>');
-        const setFields =["SINGLE_LINE_TEXT", "RICH_TEXT", "MULTI_LINE_TEXT"];
-        const FieldSorted = Object.values(GETFIELDS.properties).sort((a, b) => {return a.code.localeCompare(b.code)});
-
-       await $.each(FieldSorted, function (item, textField) {
+        //Add option fields to dropdown 
+        await $.each(FieldSorted, function (item, textField) {
             if (setFields.includes(textField.type)) {
-                option.attr("fieldName", textField.label);
-                option.attr("value", textField.code);
-                option.attr("type", textField.type);
-                option.text(`${textField.label} (${textField.code})`);
-                $('#select_field_column').append(option.clone());
+                $('#select_field_column').append($('<option>', {
+                    fieldName: textField.label,
+                    value: textField.code,
+                    type: textField.type,
+                    text: `${textField.label} (${textField.code})`
+                }));
             }
         });
-
-        //-------------------------add new row-------------------------------
+        //Add new row for LanguageList
         $(".addRowLanguageList").on("click", function () {
             createNewRow("newLanguageList", $(this));
             checkRowNumber();
         });
-        $(".removeRow").on("click", function () {
-            $(this).closest("tr").remove();
-            checkRowNumber();
-        });
-        // -------------------------add new row-------------------------------
+        //Add new row for Translate Field
         $(".addRowTranslateField").on("click", function () {
             createNewRow("newTranslateField", $(this));
             checkRowNumber();
         });
-
-        // -------------------drag and drop table row-----------------------
+        //Remove row button
+        $(".removeRow").on("click", function () {
+            $(this).closest("tr").remove();
+            checkRowNumber();
+        });
+        //Drag and drop table row
         $("#language-list-tbody").sortable({
             axis: "y",
             handle: ".drag-handle",
@@ -258,12 +237,13 @@ jQuery.noConflict();
                 ui.item.removeClass("dragging");
             },
         });
+        //Reflection button
         $(".js-reflection-button").click(function (e){
             e.preventDefault();
-            let retrieveLanguage = getLanguageList();
             // Clear options after the first option
             $('#select_default_lang_column option:gt(0)').remove();
-            $.each(retrieveLanguage, function (index, language) {
+            //Add option to Default Language dropdown
+            $.each(getLanguageList(), function (index, language) {
                 $('#select_default_lang_column').append($('<option>', {
                     value: language.languageCode,
                     text: language.language
@@ -272,7 +252,7 @@ jQuery.noConflict();
             // Set the second option as selected
             $('#select_default_lang_column option:eq(1)').prop('selected', true);
         });
-
+        //Save button
         $(".js-save-button").click(async function (e) {
             e.preventDefault();
             let config = {};
@@ -288,9 +268,10 @@ jQuery.noConflict();
             for (let i = 0; i < languageList.length; i++) {
                 allLanguageList.push(languageList[i].languageCode);
             }
+            //If both tables are verified, fail
             if (VALIDATEFIELDS == false && VALIDATELANGUAGELIST == false || VALIDATEFIELDS != VALIDATELANGUAGELIST) {return}
             // Check if the lengths of the arrays are different
-            if (allDefaultLanguage.length !== allLanguageList.length) {
+            if (allDefaultLanguage.length !== allLanguageList.length || $('.select_default_lang_column').val() == "-----") {
                 Swal10.fire({
                     icon: "error",
                     title: "Error",
@@ -299,7 +280,7 @@ jQuery.noConflict();
                 return;
             }
             for (let i = 0; i < allLanguageList.length; i++) {
-                // Check if the value is different
+                // Check if the value of languageList and defaultLanguage is different
                 if (!allDefaultLanguage.includes(allLanguageList[i])) {
                     Swal10.fire({
                         icon: "error",
@@ -309,24 +290,21 @@ jQuery.noConflict();
                     return;
                 }
             }
+            // Create config
             config = {
-                defaultLanguage: allDefaultLanguage[0],
+                defaultLanguage: $('#select_default_lang_column option:selected').val(),
                 languageList,
                 translateFields,
             };
-            console.log(config);
-
+console.log(config);
             config = JSON.stringify(config);
-            kintone.plugin.app.setConfig({config}, function() {
-                window.location.href = '../../flow?app=' + kintone.app.getId();
-            });
+            kintone.plugin.app.setConfig({config}, function() {window.location.href = '../../flow?app=' + kintone.app.getId()});
         });
-
+        //Cancel button
         $(".js-cancel-button").on("click", function () {
             window.location.href = "../../" + kintone.app.getId() + "/plugin/";
         });
-
-
+        //Set languageISO auto after selected language
         $('#select_language_column').on('change', function () {
             const selectedLanguageCode = $(this).val();
             const isoBox = $(this).closest('tr').find('#language_iso');
